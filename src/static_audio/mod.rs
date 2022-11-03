d8f9d413703884e41d9ae64550b06b36d5c904a4
Philipp-Sc <philipp.schluetermann@udo.edu>
2022-11-03 15:12:46 +0700

    name update 'Libre Learning'

--- src/static_audio/mod.rs
+++ src/static_audio/mod.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
- 
+
 pub enum StaticSounds {
     DialogWarning,
     BeginningOfLine,
@@ -50,19 +50,24 @@ impl StaticAudio {
         let array = js_sys::Uint8Array::new_with_length(length);
         array.copy_from(&sound[..]);
 
-        let context = web_sys::AudioContext::new().unwrap(); 
+        let context = web_sys::AudioContext::new().unwrap();
 
-        let source: web_sys::AudioBufferSourceNode = context.create_buffer_source().unwrap(); 
+        let source: web_sys::AudioBufferSourceNode = context.create_buffer_source().unwrap();
         let destination: web_sys::AudioDestinationNode = context.destination();
 
-        let f = Closure::once_into_js(move|buf: JsValue| { 
+        let f = Closure::once_into_js(move |buf: JsValue| {
             let array_buffer = web_sys::AudioBuffer::from(buf);
             source.set_buffer(Some(&array_buffer));
-            source.connect_with_audio_node(&destination).ok(); 
+            source.connect_with_audio_node(&destination).ok();
             source.start().ok();
         });
 
-        let _promise = context.decode_audio_data_with_success_callback(&array.buffer(),f.dyn_ref::<js_sys::Function>().unwrap()).unwrap();
+        let _promise = context
+            .decode_audio_data_with_success_callback(
+                &array.buffer(),
+                f.dyn_ref::<js_sys::Function>().unwrap(),
+            )
+            .unwrap();
         //let result = wasm_bindgen_futures::JsFuture::from(promise).await?; // async
     }
 }
