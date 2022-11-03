6b61c9e44c3ab8b3de993f155828b53f86c4005c
Philipp-Sc <philipp.schluetermann@udo.edu>
2022-11-03 10:55:44 +0700

    removed native target

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -1,8 +1,10 @@
+/*
 #[cfg(not(target_arch = "wasm32"))]
 use std::io::Cursor;
 
 #[cfg(not(target_arch = "wasm32"))]
 use rodio::{source::Source, Decoder, OutputStream};
+*/
 
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)]
@@ -45,8 +47,8 @@ impl AudioItem {
         #[cfg(target_arch = "wasm32")]
         self.play_audio_web();
 
-        #[cfg(not(target_arch = "wasm32"))]
-        self.play_audio_native();
+        /*#[cfg(not(target_arch = "wasm32"))]
+        self.play_audio_native();*/
     }
 
     #[cfg(target_arch = "wasm32")]
@@ -64,46 +66,47 @@ impl AudioItem {
             },
         }
     }
+    /*
+        #[cfg(not(target_arch = "wasm32"))]
+        fn play_audio_native(&mut self) {
+            //  BackendSpecificError { description: "ALSA function 'snd_pcm_open' failed with error 'EHOSTDOWN: Host is down'" }
+            // TODO: create docker container with alsa support.
 
-    #[cfg(not(target_arch = "wasm32"))]
-    fn play_audio_native(&mut self) {
-        //  BackendSpecificError { description: "ALSA function 'snd_pcm_open' failed with error 'EHOSTDOWN: Host is down'" }
-        // TODO: create docker container with alsa support.
+            let content: Option<std::io::Cursor<bytes::Bytes>>;
 
-        let content: Option<std::io::Cursor<bytes::Bytes>>;
-
-        if self.audio_data.is_none() {
-            let request = ehttp::Request::get(&self.audio_url);
-            let response = ehttp::fetch_blocking(&request);
-            match response {
-                Ok(response) => match response.ok {
-                    true => {
-                        self.audio_data = Some(bytes::Bytes::from(response.bytes));
-                        content = Some(Cursor::new(self.audio_data.as_ref().unwrap().clone()));
-                    }
-                    false => {
+            if self.audio_data.is_none() {
+                let request = ehttp::Request::get(&self.audio_url);
+                let response = ehttp::fetch_blocking(&request);
+                match response {
+                    Ok(response) => match response.ok {
+                        true => {
+                            self.audio_data = Some(bytes::Bytes::from(response.bytes));
+                            content = Some(Cursor::new(self.audio_data.as_ref().unwrap().clone()));
+                        }
+                        false => {
+                            content = None;
+                        }
+                    },
+                    Err(_) => {
                         content = None;
                     }
-                },
-                Err(_) => {
-                    content = None;
+                }
+            } else {
+                content = Some(Cursor::new(self.audio_data.as_ref().unwrap().clone()));
+            }
+            match content {
+                Some(content) => {
+                    // Get a output stream handle to the default physical sound device
+                    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+                    // Decode that sound file into a source
+                    let source = Decoder::new(content).unwrap();
+                    // Play the sound directly on the device
+                    stream_handle.play_raw(source.convert_samples()).ok();
+                }
+                None => {
+                    // error
                 }
             }
-        } else {
-            content = Some(Cursor::new(self.audio_data.as_ref().unwrap().clone()));
         }
-        match content {
-            Some(content) => {
-                // Get a output stream handle to the default physical sound device
-                let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-                // Decode that sound file into a source
-                let source = Decoder::new(content).unwrap();
-                // Play the sound directly on the device
-                stream_handle.play_raw(source.convert_samples()).ok();
-            }
-            None => {
-                // error
-            }
-        }
-    }
+    */
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #![warn(clippy::all, rust_2018_idioms)]
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+//#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 // When compiling natively:
+/*
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
     // Log to stdout (if you run with `RUST_LOG=debug`).
@@ -13,7 +15,7 @@ fn main() {
         native_options,
         Box::new(|cc| Box::new(eframe_template::LibreLearningApp::new(cc))),
     );
-}
+}*/
 
 // when compiling to web using trunk.
 #[cfg(target_arch = "wasm32")]
