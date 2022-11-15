6b369ab9b218ef4ed997292e218d29b3f3953423
Philipp-Sc <philipp.schluetermann@tu-dortmund.de>
2022-11-15 04:31:49 +0000

    Deploying to gh-pages from @ Philipp-Sc/librelearning@821055ee30807399d2de583e46c5179ec51b6235 🚀

---
+++ snippets/libre_learning-e82c1751667cc20d/defined-in-js.js
@@ -0,0 +1,94 @@
+
+export function is_ready(){ 
+  console.log("App is ready.");
+  document.getElementById("center_text").remove();
+
+  // workaround for https://github.com/emilk/egui/issues/2231
+  document.getElementById("the_canvas_id").classList.add("top-always-zero-percent");
+
+}
+
+/**
+ * ebisu.js placed in assets:
+ *  - assets/ebisu.min.es6.js (see https://raw.githubusercontent.com/fasiha/ebisu.js)
+ * 
+ * ./index.html edited to include:
+ * 
+ *  ```
+ *  <link data-trunk rel="copy-file" href="assets/ebisu.min.es6.js" />
+ * 
+ *  <script type="text/javascript" src="ebisu.min.es6.js"></script>
+ *  ```
+ * 
+ * TODO: Port EBISU to Rust
+ */
+export class SpacedRepetition {
+
+    model_dict; 
+    default_half_life_time;
+
+    constructor() { 
+        this.model_dict = {};  
+        this.default_half_life_time = 1000*60*30;  
+    }
+ 
+    #transformEBISUModel(ebisu_model, prev_timestamp, timestamp, score){
+      if(ebisu_model==null){ // score is null
+        return [3,3,this.default_half_life_time]
+      }else{
+        var elapsedTime = timestamp - prev_timestamp;  // assertion: timestamp >= prev_timestamp  
+        return ebisu.updateRecall(ebisu_model, score,1, elapsedTime);
+      }
+    }  
+ 
+    #getEBISUModel(card_id, timestamps, scores){
+      // load model
+      var model = this.model_dict["EBISU_"+card_id+"_"+timestamps.length];
+      
+      if(!model){ 
+        // first creation of model
+        model = this.#transformEBISUModel(null,null,null,scores[0]); 
+        for(var t=1;t<=timestamps.length-1;t++){ 
+          model = this.#transformEBISUModel(
+              model,
+              timestamps[t-1],
+              timestamps[t], 
+              scores[t]);
+        }
+        // save model
+        this.model_dict["EBISU_"+card_id+"_"+timestamps.length]=model;
+      }
+      return model;
+    }
+
+    #predictedHalfLifeEBISU(card_id,timestamps,scores,recall_probability,ebisu_model){
+      var model = ebisu_model;
+      if(model==null){
+        model = this.#getEBISUModel(card_id,timestamps,scores)
+      }
+      return ebisu.modelToPercentileDecay(model, recall_probability, false, 1e-4);
+    }
+
+    /**
+     * This function returns a date at which the card should be reviewed again.
+     * The best usage of this is to decide the order of the cards to be reviewed. 
+     * @param {*} card_id 
+     * @param {*} timestamps = [t1,t2,t3,..,tn]
+     * @param {*} scores = [s1,s2,s3,..,sn] 
+     * @returns String 
+     */
+    calculateEBISU(card_id, timestamps, scores){   
+      //console.assert(timestamps.length>0, timestamps);        
+      //console.assert(scores.length>0, scores);     
+
+      if (timestamps.length==0 || scores.length==0) {
+        //console.log("returning new Date().getTime()");
+        return new Date().getTime();
+      }
+
+      return new Date(
+        timestamps[timestamps.length-1] + this.#predictedHalfLifeEBISU(card_id,timestamps,scores,0.5,null) // timeToElapseForHalfLife
+         ).getTime();
+    } 
+}
+     
---
+++ sw.js
@@ -0,0 +1,26 @@
+var cacheName = 'libre-learning-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './libre_learning.js',
+  './libre_learning_bg.wasm', 
+  './ebisu.min.es6.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
